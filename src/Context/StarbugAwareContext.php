e7d7f263ecb9f57169030689cade40402d2de3ef
Ali Gangji <ali@neonrain.com>
2018-11-11 12:20:09 -0700

    missing interface method

--- src/Context/StarbugAwareContext.php
+++ src/Context/StarbugAwareContext.php
@@ -2,7 +2,8 @@
 namespace Starbug\Behat\Context;
 
 use Behat\Behat\Context\Context;
+use Interop\Container\ContainerInterface;
 
 interface StarbugAwareContext extends Context {
-
+  public function setStarbugContainer(ContainerInterface $container);
 }
