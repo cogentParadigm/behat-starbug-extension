e97b8275e77add462b0957c374e10404a4ad90b1
Ali Gangji <ali@neonrain.com>
2019-10-03 17:19:55 -0600

    add support for older Starbug versions which use Starbug\Core\URL instead of Starbug\Http\Url

--- src/ServiceContainer/StarbugLoader.php
+++ src/ServiceContainer/StarbugLoader.php
@@ -1,8 +1,10 @@
 <?php
 namespace Starbug\Behat\ServiceContainer;
 
+use Exception;
 use Starbug\Core\ContainerFactory;
-use Starbug\Http\Url;
+use Starbug\Http\Url as HttpUrl;
+use Starbug\Core\URL as CoreUrl;
 
 class StarbugLoader {
   protected $baseUrl;
@@ -17,10 +19,19 @@ class StarbugLoader {
     $container = $factory->build([]);
     date_default_timezone_set($container->get('time_zone'));
     $components = parse_url($this->baseUrl);
-    $url = new Url($components["host"], $container->get("website_url"));
-    $url->setScheme($components["scheme"]);
-    $request = $container->make("Starbug\Http\Request", ['url' => $url]);
-    $container->set("Starbug\Http\RequestInterface", $request);
+    if (class_exists("Starbug\Http\Url")) {
+      $url = new HttpUrl($components["host"], $container->get("website_url"));
+      $url->setScheme($components["scheme"]);
+      $request = $container->make("Starbug\Http\Request", ['url' => $url]);
+      $container->set("Starbug\Http\RequestInterface", $request);
+    } else if (class_exists("Starbug\Core\URL")) {
+      $url = new CoreUrl($components["host"], $container->get("website_url"));
+      $url->setScheme($components["scheme"]);
+      $request = $container->make("Starbug\Core\Request", ['url' => $url]);
+      $container->set("Starbug\Core\RequestInterface", $request);
+    } else {
+      throw new Exception("Unable to bootstrap Starbug. Cannot find suitable UrlInterface.");
+    }
     return $container;
   }
 }
