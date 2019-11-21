747ae72689a5ec74dca71db1bd013d866da8c3ae
Ali Gangji <ali@neonrain.com>
2019-11-21 10:12:07 -0700

    Fix to waitForDom in ChromeDriver

--- src/ChromeDriver/ChromeDriver.php
+++ src/ChromeDriver/ChromeDriver.php
@@ -5,12 +5,10 @@ use DMore\ChromeDriver\ChromeDriver as ParentDriver;
 
 class ChromeDriver extends ParentDriver {
   protected function waitForDom() {
-    if (!$this->page->hasJavascriptDialog()) {
-      $this->wait($this->domWaitTimeout, 'document.readyState == "complete"');
-      $this->page->waitForLoad();
-      if ($this->evaluateScript('document.querySelector("script[src$=\"/dojo.js\"]") != null')) {
-        $this->wait(10000, "typeof require != 'undefined'");
-        $script = <<<JS
+    parent::waitForDom();
+    if ($this->evaluateScript('document.querySelector("script[src$=\"/dojo.js\"]") != null')) {
+      $this->wait(10000, "typeof require != 'undefined'");
+      $script = <<<JS
   window.pageIsReady = false;
   require(["dojo/ready", "dojo/parser"], function(ready, config) {
     ready(function(){
@@ -18,9 +16,8 @@ class ChromeDriver extends ParentDriver {
     });
   });
 JS;
-        $this->executeScript($script);
-        $this->wait(10000, "window.pageIsReady == true");
-      }
+      $this->executeScript($script);
+      $this->wait(10000, "window.pageIsReady == true");
     }
   }
 }
